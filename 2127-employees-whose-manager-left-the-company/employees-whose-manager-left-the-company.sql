ae6c784be2183489f6f6462a1de4a490121e3671
Anirban Maity <157023094+Ani2005maity@users.noreply.github.com>
2026-08-21 16:24:39 +0530

    Time: 411 ms (27.65%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -2,10 +2,5 @@
 select employee_id
 from employees e
 where salary < 30000
-and manager_id is not null
-and not exists (
-select 1
-from employees m
-where m.employee_id = e.manager_id
-)
+and manager_id not in (select employee_id from employees)
 order by employee_id;
